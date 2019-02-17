f83752719b254c5b8b49fcb0bd0e254ed99acab8
SravantiNomula <47375870+SravantiNomula@users.noreply.github.com>
2019-02-17 08:06:08 +0530

    Create server.c

---
+++ ASSIGNMENT-1/EchoEngine/server.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define SERVER_KEY_PATHNAME "/tmp/mqueue_server_key"
+#define PROJECT_ID 'M'
+#define QUEUE_PERMISSIONS 0660
+
+struct message_text {
+    int qid;
+    char buf [200];
+};
+
+struct message {
+    long message_type;
+    struct message_text message_text;
+};
+
+int main (int argc, char **argv)
+{
+    key_t msg_queue_key;
+    int qid;
+    struct message message;
+
+    if ((msg_queue_key = ftok (SERVER_KEY_PATHNAME, PROJECT_ID)) == -1) {
+        perror ("ftok");
+        exit (1);
+    }
+
+    if ((qid = msgget (msg_queue_key, IPC_CREAT | QUEUE_PERMISSIONS)) == -1) {
+        perror ("msgget");
+        exit (1);
+    }
+
+    printf ("Server: Hello, World!\n");
+
+    while (1) {
+        // read an incoming message
+        if (msgrcv (qid, &message, sizeof (struct message_text), 0, 0) == -1) {
+            perror ("msgrcv");
+            exit (1);
+        }
+
+        printf ("Server: message received.\n");
+
+        // process message
+        
+	//int length = strlen (message.message_text.buf);
+        char buff [20];
+        strcpy(buff,message.message_text.buf);
+        
+	int c=0;
+   	char ch;
+   while (buff[c] != '\0') {
+      ch = buff[c];
+      if (ch >= 'A' && ch <= 'Z')
+         buff[c] = buff[c] + 32;
+      else if (ch >= 'a' && ch <= 'z')
+         buff[c] = buff[c] - 32;  
+      c++;  
+   }
+   
+  // printf("The case changed string is: %s\n", buff);
+
+
+
+
+
+
+
+	strcpy (message.message_text.buf, buff);
+	int client_qid = message.message_text.qid;
+        message.message_text.qid = qid;
+
+        // send reply message to client
+        if (msgsnd (client_qid, &message, sizeof (struct message_text), 0) == -1) {  
+            perror ("msgget");
+            exit (1);
+        }
+
+        printf ("Server: response sent to client.\n");
+    }
+}
